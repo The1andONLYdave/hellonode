b0e7d78e1426832e7bcc18ea63634c5242cac977
David-Lee Kulsch <david-lee.kulsch@saitow.ag>
2019-01-29 12:52:01 +0100

    fixing

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("HeBROKENllo David\n");
+  response.end("Hello David\n");
 });
 
 // listen on localhost:8000
